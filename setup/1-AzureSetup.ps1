fb5d0fa77f067d7ffcc133fd6cf1e5a3cf0f5427
Dimitri Koens <40596624+Dimtemp@users.noreply.github.com>
2021-04-21 16:02:41 +0200

    Update 1-AzureSetup.ps1

--- setup/1-AzureSetup.ps1
+++ setup/1-AzureSetup.ps1
@@ -2,27 +2,35 @@
 $students = 'student1', 'student2'
 $vmsize = 'Standard D4s v3'
 $location = 'westeurope'
+$containerName = 'files'
 $sourceFiles = 'Q:\Images\Parent\Base17A-W10-1607.vhd', 'Q:\Images\Parent\Base17C-WS16-1607.vhd', 'Q:\Software\Microsoft\SQL\en_sql_server_2016_enterprise_with_service_pack_1_x64_dvd_9542382.iso', 'Q:\Software\Microsoft\System Center 2019\mu_system_center_operations_manager_2019_x64_dvd_b3488f5c.iso'
+
 
 # check input files
 $sourceFiles | foreach-object { if (!(Test-Path $_)) { Throw "Source file does not exist! Terminating... $_" } }
 
+
 # check for AzCopy existence
 if (!(Get-Command AzCopy.exe)) { throw "AzCopy does not exist! Terminating." }
 
+
 # create storage account
 $sa = New-AzStorageAccount -ResourceGroupName SCOMTraining -Name "stor$(Get-Random)" -SkuName Standard_LRS -Location 'westeurope' -Kind StorageV2
-New-AzStorageContainer -Context $sa.Context -Name 'container1' -Permission Container
+New-AzStorageContainer -Context $sa.Context -Name $containerName -Permission Container
+
 
 # generate SAS
 $StartTime = Get-Date
-$EndTime = $startTime.AddDays(365)
-$blobSas = New-AzStorageContainerSASToken -Name 'container1' -Permission rwd -StartTime $StartTime -ExpiryTime $EndTime -context $sa.Context
+$ExpiryTime = $startTime.AddDays(365)
+$blobSas = New-AzStorageContainerSASToken -Name $containerName -Permission rwd -StartTime $StartTime -ExpiryTime $ExpiryTime -context $sa.Context
 
 
 # upload files to storage account
 $sourceFiles | Foreach { AzCopy copy $_ $blobSas }
 
+
 # create VM in Azure for each student
 UITWERKEN
-$students | foreach-Object { New-AzVM -Resourcegroup $_  -VMName $_ -location $location -vmsize $vmsize }
+$students | Foreach-Object {
+    New-AzVM -Resourcegroup $_  -VMName $_ -location $location -vmsize $vmsize
+}
