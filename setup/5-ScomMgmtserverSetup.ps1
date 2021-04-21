94dfadb9e4889bc711bf7a3c836cb0d6bcf085b8
Dimitri Koens <40596624+Dimtemp@users.noreply.github.com>
2021-04-21 16:11:01 +0200

    Update and rename 5-scomsetup.ps1 to 5-ScomMgmtserverSetup.ps1

--- setup/5-ScomMgmtserverSetup.ps1
+++ setup/5-ScomMgmtserverSetup.ps1
@@ -1,12 +1,17 @@
-# optionally start setup procedure
-
+#init
+$ADAccounts = 'MSAA', 'SDK', 'DRA', 'DWA'
+$pw = ConvertTo-SecureString 'Pa55w.rd' -AsPlainText -Force
 $scomSetup = 'C:\System Center Operations Manager 2019\Setup.exe'
 
-if (Test-Path $scomSetup) { Write-Host "SCOM Setup files found." } else { throw "SCOM Setup files not found! Aborting." }
+if (Test-Path $scomSetup) {
+    Write-Host "SCOM Setup files found."
+} else {
+    throw "SCOM Setup files not found! Aborting."
+}
 
-New-ADOrganizationalUnit SCOM
-$pw = ConvertTo-SecureString 'Pa55w.rd' -AsPlainText -Force
-'MSAA', 'SDK', 'DRA', 'DWA' | foreach {
+# prepare AD
+New-ADOrganizationalUnit 'SCOM'
+$ADAccounts | foreach-object {
     New-ADUser -AccountPassword $pw -Name $_ -Path 'OU=SCOM,DC=Adatum,DC=msft' -Enabled $true
 }
 ADD-ADGroupMember -Identity 'Domain Admins' -Members 'MSAA', 'SDK'
@@ -19,17 +24,18 @@ Start-Job {
     Start-Process -FilePath $scomSetup -ArgumentList '/silent /install /components:OMServer,OMConsole,OMReporting /ManagementGroupName:OM1 /SRSInstance:VAN-OM1 /SqlServerInstance:VAN-OM1 /DatabaseName:OperationsManager /DWSqlServerInstance:VAN-OM1 /DWDatabaseName:OperationsManagerDW /ActionAccountUser:Adatum\MSAA /ActionAccountPassword:Pa$$w0rd /DASAccountUser:Adatum\SDK /DASAccountPassword:Pa$$w0rd /DatareaderUser:Adatum\DRA /DatareaderPassword:Pa$$w0rd /DataWriterUser:Adatum\DWA /DataWriterPassword:Pa$$w0rd /EnableErrorReporting:Always /SendODRReports:1 /SendCEIPReports:0 /UseMicrosoftUpdate:1 /AcceptEndUserLicenseAgreement:1'
 }
 
+
 $start = Get-Date
-$p1 = gps
+$p1 = Get-Process
 Do {
-  Sleep 1
-  $p2 = gps
-  Compare $p1 $p2 -Property id -passthru | % {
-    $msg = "{0} {1,5} pid {2}  {3}" -f ((Get-Date)-$start).ToString(), $_.id, $_.name, $_.path
-    if ($_.sideIndicator -eq "=>") { Write-Host $msg -fore green  }
-    if ($_.sideIndicator -eq "<=") { Write-Host $msg -fore yellow }
-  }
-  $p1 = $p2
+    Sleep 1
+    $p2 = gps
+    Compare $p1 $p2 -Property id -passthru | % {
+        $msg = "{0} {1,5} pid {2}  {3}" -f ((Get-Date)-$start).ToString(), $_.id, $_.name, $_.path
+        if ($_.sideIndicator -eq "=>") { Write-Host $msg -fore green  }
+        if ($_.sideIndicator -eq "<=") { Write-Host $msg -fore yellow }
+    }
+    $p1 = $p2
 } until ( $p2 | where { $_.name -match 'monitoringhost' } )
 
 Write-Host -fore green 'health service started!'
