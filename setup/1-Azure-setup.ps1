ce4e8acf83dc4c8f3b6001d94a814270697f00b8
Dimitri Koens <40596624+Dimtemp@users.noreply.github.com>
2021-04-24 10:57:29 +0200

    Update 1-Azure-setup.ps1

--- setup/1-Azure-setup.ps1
+++ setup/1-Azure-setup.ps1
@@ -3,13 +3,15 @@ $students = 'student1', 'student2'
 $resourceGroupName = 'SCOMTraining'
 $vmsize = 'Standard_D4s_v3'
 $location = 'westeurope'
-$containerName = 'files'
+$containerName = 'files2'
 $secStringPassword = ConvertTo-SecureString 'Pa55w.rd1234' -AsPlainText -Force
+$sourceFilesPath = 'Q:\Images\SC2019'
 $sourceFiles = 'Q:\Images\Parent\Base17A-W10-1607.vhd', 'Q:\Images\Parent\Base17C-WS16-1607.vhd', 'Q:\Software\Microsoft\SQL\en_sql_server_2016_enterprise_with_service_pack_1_x64_dvd_9542382.iso', 'Q:\Software\Microsoft\System Center 2019\mu_system_center_operations_manager_2019_x64_dvd_b3488f5c.iso'
 
 
 # check input files
 $sourceFiles | foreach-object { if (!(Test-Path $_)) { Throw "Source file does not exist! Terminating... $_" } }
+if (!(Test-Path $sourceFilesPath)) { Throw "Source file does not exist! Terminating... $_" }
 
 
 # check for AzCopy existence
@@ -20,16 +22,19 @@ if (!(Get-Command AzCopy.exe)) { throw "AzCopy does not exist! Terminating." }
 $sa = Get-AzStorageAccount -ResourceGroupName $resourceGroupName
 if ($null -eq $sa) {
     $sa = New-AzStorageAccount -ResourceGroupName $resourceGroupName -Name "stor$(Get-Random)" -SkuName Standard_LRS -Location 'westeurope' -Kind StorageV2
-    New-AzStorageContainer -Context $sa.Context -Name $containerName -Permission Container
-
-    # generate SAS
-    $StartTime = Get-Date
-    $ExpiryTime = $startTime.AddDays(365)
-    $blobSas = New-AzStorageContainerSASToken -Name $containerName -Permission rwd -StartTime $StartTime -ExpiryTime $ExpiryTime -context $sa.Context
-
-    # upload files to storage account
-    $sourceFiles | Foreach { AzCopy copy $_ $blobSas }
 }
+
+
+New-AzStorageContainer -Context $sa.Context -Name $containerName -Permission Container
+
+# generate SAS
+$StartTime = Get-Date
+$ExpiryTime = $startTime.AddDays(365)
+$blobSas = New-AzStorageContainerSASToken -Name $containerName -Permission rwd -StartTime $StartTime -ExpiryTime $ExpiryTime -context $sa.Context
+
+# upload files to storage account
+AzCopy copy $sourceFilesPath $blobSas --recurse
+$sourceFiles | Foreach { AzCopy copy $_ $blobSas }
 
 
 # create VM in Azure for each student
